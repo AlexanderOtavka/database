82a611e39fb78166786904b8bb163db43c6e906f
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-01-30 18:04:41 -0800

    Add jinja template and remvoe excess code.

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import cgi
+from google.appengine.api import users
 import webapp2
+import jinja2
+import os
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__) + "/client"))
 
 class MainPage(webapp2.RequestHandler):
-  def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Hello World')
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
+
+class Display_Data(webapp2.RequestHandler):
+    def post(self):
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
-  ('/', MainPage),
+    ('/', MainPage),
+    ('/add', Display_Data),
 ], debug=True)
-
