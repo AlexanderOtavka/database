c71a3e045685ede653b3a06fbd6e45a591dec469
Duncan Grubbs <duncan.grubbs@gmail.com>
2016-01-30 22:56:50 -0800

    Random changes.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import cgi
-from google.appengine.api import users
+from google.appengine.ext import ndb
 import webapp2
 import jinja2
 import os
@@ -13,7 +13,17 @@ class MainPage(webapp2.RequestHandler):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render())
 
+class About_Page(webapp2.RequestHandler):
+    def get(self):
+        template = JINJA_ENVIRONMENT.get_template('about.html')
+        self.response.write(template.render())
+
 class Display_Data(webapp2.RequestHandler):
+    content = nbd.StringProperty()
+    model = content.Display_Data(content='foo')
+    model.content = "bar"
+
+    content = self.request.get()
     def post(self):
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render())
@@ -21,4 +31,5 @@ class Display_Data(webapp2.RequestHandler):
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/add', Display_Data),
+    ('/about', About_Page),
 ], debug=True)
